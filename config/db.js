15b43d909e396be5377fb8d475862f455b4870d9
Hải <142956982+haiztvn@users.noreply.github.com>
2024-12-21 00:15:03 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2');
 
 // Tạo một Connection Pool
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'doanchuyennghanh',
+    host: "gamehay.id.vn",
+    user: "ndbdxcjw_doanchuyennganh",
+    password: "YcuDSH8P5nWaxGuzYebR",
+    database: "ndbdxcjw_doanchuyennganh",
     waitForConnections: true,
-    connectionLimit: 10, // Giới hạn số kết nối tối đa
+    connectionLimit: 10,  // Giới hạn số kết nối tối đa trong pool
     queueLimit: 0
 });
 
